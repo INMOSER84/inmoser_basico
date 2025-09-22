a7f501f1c8eb4a3e5a4efceb332e9889530b69e3
baruc <baruc@DirGeneral.localdomain>
2025-09-21 19:31:02 -0600

    🔄 Actualización completa del módulo inmoser_service_order

--- inmoser_service_order/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import res_partner
-from . import hr_employee
-from . import service_equipment
-from . import service_type
-from . import service_order
-from . import service_order_refaction_line
--- inmoser_service_order/wizards/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import service_order_wizard
